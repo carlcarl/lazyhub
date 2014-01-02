aa33ad48a57dedcac8d749f1ce5e1255ba19b26a
carlcarl <carlcarlking@gmail.com>
2014-01-03 03:00:38 +0800

    Remove admin url

--- lazyhub/urls.py
+++ lazyhub/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
 
-from django.contrib import admin
-admin.autodiscover()
+# from django.contrib import admin
+# admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'lazyhub.views.index', name='index'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 )
