82d53aaf1b2ceb25f62025148b8531071b27eb3e
carlcarl <carlcarlking@gmail.com>
2015-08-28 11:50:30 +0800

    Remove unused DB setting

--- lazyhub/settings.py
+++ lazyhub/settings.py
@@ -69,12 +69,12 @@ WSGI_APPLICATION = 'lazyhub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
